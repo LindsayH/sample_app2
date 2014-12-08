34680fc204c4ce0bfc35b01d15566e95cc7b57a3
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-08 09:31:20 -0700

    added verification that the user emails is downcase and updated the regx for to not allow doble dots

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 class User < ActiveRecord::Base
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	before_save { self.email = email.downcase }
+	# Validates username is present, and is no longer than 50 charicters
 	validates :name,  presence: true, length: { maximum: 50 }
+	#valitdates user email is present, is no longer than 255 charicters, and also uses an regx.
+	
 	validates :email, presence: true, length: { maximum: 255 },format: { with: VALID_EMAIL_REGEX },
 	 					                    uniqueness: { case_sensitive: false }
-
-
 
 	has_secure_password
 	validates :password, length: { minimum: 6 }
