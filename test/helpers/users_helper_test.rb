650d921ac34955b01a7c5b78c8e3a9208f798316
Lindsay Hammon <lindsay.work.mail@gmail.com>
2014-12-10 14:36:23 -0700

    on to chapter 8

--- test/helpers/users_helper_test.rb
+++ test/helpers/users_helper_test.rb
@@ -1,4 +1,13 @@
 require 'test_helper'
 
 class UsersHelperTest < ActionView::TestCase
+
+	# def initialize 
+	# 	@user = User.new{name:linday, email:lindsay@gmail.com}
+	# end
+	# 	assert_includs gravitar_for(@user)
+	 # test "gravitar_for take an option for size" do
+		# gravatar_for, user={params[:id]}, size:120
+		# assert_response :success
+ 	#  end 
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,5 +20,9 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
+    assert_not flash.nil?
+    
+    # assert_select 'div#error_explanation' 
+    # assert_select 'div.alert alert-danger'
   end
 end
